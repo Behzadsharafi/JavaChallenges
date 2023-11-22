542904cb80ce4ff1615a1b8bb409bed5ffa7735a
Behzad Sharafi <behzadsharafi@gmail.com>
2023-11-22 19:52:31 +1100

    adds solutions

--- src/javaChallenges/JavaChallenges.java
+++ src/javaChallenges/JavaChallenges.java
@@ -1,6 +1,7 @@
 package javaChallenges;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -284,5 +285,58 @@ public class JavaChallenges {
 
 		throw new IllegalArgumentException("No outlier found.");
 	}
+	
+	
+//	You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+//
+//	Examples:
+//	strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+//
+//	Concatenate the consecutive strings of strarr by 2, we get:
+//
+//	treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+//	folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+//	trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+//	blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+//	abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+//
+//	Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+//	The first that came is "folingtrashy" so 
+//	longest_consec(strarr, 2) should return "folingtrashy".
+//
+//	In the same way:
+//	longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+//	n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+//
+//	Note
+//	consecutive strings : follow one after another without an interruption
+	
+	  public static String longestConsec(String[] strarr, int k) {
+	        if (strarr.length == 0 || k > strarr.length || k <= 0)
+	            return "";
+
+	        String longestStr = "";
+	        for (int index = 0; index < strarr.length - k + 1; index++) {
+	            StringBuilder sb = new StringBuilder();
+	            for (int i = index; i < index + k; i++) {
+	                sb.append(strarr[i]);
+	            }
+	            if (sb.toString().length() > longestStr.length()) {
+	                longestStr = sb.toString();
+	            }
+	        }
+	        return longestStr;
+	    }
+	  
+	  public static String longestConsec2(String[] strarr, int k) {
+	        if (k <= 0) {
+	            return "";
+	        }
+
+	        return IntStream.rangeClosed(0, strarr.length - k)
+	                .mapToObj(i -> Arrays.stream(strarr, i, i + k).collect(Collectors.joining()))
+	                .max(Comparator.comparingInt(String::length))
+	                .orElse("");
+	    }
 
 }
--- src/javaChallenges/Main.java
+++ src/javaChallenges/Main.java
@@ -1,17 +1,40 @@
 package javaChallenges;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
 
-	public static int findOutlier(int[] array) {
+	public static String longestConsec(String[] strarr, int k) {
+		   int n = strarr.length;
 
+	        // Check for valid input
+	        if (n == 0 || k > n || k <= 0) {
+	            return "";
+	        }
+
+	        String longestString = "";
+
+	        for (int i = 0; i <= n - k; i++) {
+	            StringBuilder currentString = new StringBuilder();
+	            for (int j = i; j < i + k; j++) {
+	                currentString.append(strarr[j]);
+	            }
+
+	            if (currentString.length() > longestString.length()) {
+	                longestString = currentString.toString();
+	            }
+	        }
+
+	        return longestString;
 	}
 
 	public static void main(String[] args) {
 		// Example
-		int[] example1 = { 2, 4, 0, 100, 4, 11, 2602, 36 };
-		System.out.println(findOutlier(example1));
+		String[] example1 = { "tree", "foling", "trashy", "blue", "abcdef", "uvwxyz" };
+		System.out.println(longestConsec(example1, 0));
 	}
 
 }
+
+//return Arrays.stream(strarr).skip(1).limit(2).collect(Collectors.joining());
