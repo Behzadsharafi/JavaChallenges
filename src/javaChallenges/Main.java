f3e90cdb17db67f6ac6f4e3bc21da8459348cae5
Behzad Sharafi <behzadsharafi@gmail.com>
2023-12-05 22:52:53 +1100

    adds solutions

--- src/javaChallenges/Main.java
+++ src/javaChallenges/Main.java
@@ -4,24 +4,99 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
 
-	public static int countSmileys(List<String> arr) {
-		return (int) arr.stream().filter(x -> x.matches("[:;][-~]?[)D]")).count();
-	}
+	public static int parseInt(String numStr) {
+        String[] numArray = numStr.split("[ |-]");
+        int number = 0;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("zero", 0);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.put("six", 6);
+        map.put("seven", 7);
+        map.put("eight", 8);
+        map.put("nine", 9);
+        map.put("ten", 10);
+        map.put("eleven", 11);
+        map.put("twelve", 12);
+        map.put("thirteen", 13);
+        map.put("fourteen", 14);
+        map.put("fifteen", 15);
+        map.put("sixteen", 16);
+        map.put("seventeen", 17);
+        map.put("eighteen", 18);
+        map.put("nineteen", 19);
+        map.put("twenty", 20);
+        map.put("thirty", 30);
+        map.put("forty", 40);
+        map.put("fifty", 50);
+        map.put("sixty", 60);
+        map.put("seventy", 70);
+        map.put("eighty", 80);
+        map.put("ninety", 90);
+        map.put("hundred", 100);
+        map.put("thousand", 1000);
+        map.put("million", 1000000);
+        
+        for (int i = 0; i < numArray.length; i++) {
+            for (String key : map.keySet()) {
+                if (numArray[i].toLowerCase().equals(key)) {
+                    if (map.get(key) == 100) {
+                        int temp = number % 100;
+                        number -= temp;
+                        number += temp * (map.get(key));
+                    }
+                    else if (map.get(key) > 100)
+                        number *= (map.get(key));
+                    else
+                        number += map.get(key);
+                      break;
+                }
+            }
+        }
+        return number;
+    }
 
 	public static void main(String[] args) {
-		List<String> a = new ArrayList<String>();
-		a.add(":)");
-		a.add(":D");
-		a.add(":-}");
-		a.add(":-()");
+	
 
-		System.out.println(countSmileys(a));
+		System.out.println(parseInt("seven hundred eighty-three thousand nine hundred and nineteen"));
 	}
 }
+
+//int number = 0;
+
+
+ //i=0
+//-----------------
+//number=2
+
+//i=1
+//-----------------
+//temp=2
+//number=2-2=0
+//number= 0 + 2*100= 200
+
+//i=2
+//-----------------
+//nothing
+
+//i=3
+//-----------------
+//number = 200+60 = 260
+
+//i=4
+//-----------------
+
+
