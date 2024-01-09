eff4151c17fc26189cb1adfcc0be9b6dd00f78fe
Behzad Sharafi <behzadsharafi@gmail.com>
2024-01-09 18:44:50 +1100

    adds solution

--- src/javaChallenges/JavaChallenges.java
+++ src/javaChallenges/JavaChallenges.java
@@ -1,5 +1,6 @@
 package javaChallenges;
 
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -659,5 +660,27 @@ public class JavaChallenges {
 		  return n[1]/3*1000 + n[6]/3*600+ n[5]/3*500+ n[4]/3*400+n[3]/3*300+ n[2]/3*200+ n[1]%3*100+n[5]%3*50;
 
 		  }
+	 
+	 
+//	 Your goal in this kata is to implement a difference function, which 
+//	 subtracts one list from another and returns the result.
+//   It should remove all values from list a, which are present in list b keeping their order.
+//   Kata.arrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+//	 If a value is present in b, all of its occurrences must be removed from the other:
+//   Kata.arrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2}) => new int[] {1, 3}
+	 
+	 public static int[] arrayDiff1(int[] a, int[] b) {
+		   List<Integer> listA = Arrays.stream(a).boxed().collect(Collectors.toList());
+	        List<Integer> listB = Arrays.stream(b).boxed().collect(Collectors.toList());
+	        listA.removeAll(listB);
+	        return listA.stream().mapToInt(e -> e).toArray();
+	    }
+	 
+	 public static int[] arrayDiff2(int[] a, int[] b) {
+		    return IntStream.of(a).filter(x -> IntStream.of(b).noneMatch(y -> y == x)).toArray();
+		  }
+	 
+	
+
 
 }
--- src/javaChallenges/Main.java
+++ src/javaChallenges/Main.java
@@ -1,48 +1,28 @@
 package javaChallenges;
 
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
-	
+	public static int[] arrayDiff(int[] a, int[] b) {
+		return IntStream.of(a).filter(x -> IntStream.of(b).noneMatch(y -> y == x)).toArray();
+	}
 
-	  public static int greedy(int[] dice) {
-		  int n[]= new int[7];
-		  for (int d:dice) n[d]++;
-		  return n[1]/3*1000 + n[6]/3*600+ n[5]/3*500+ n[4]/3*400+n[3]/3*300+ n[2]/3*200+ n[1]%3*100+n[5]%3*50;
+	public static void main(String[] args) {
 
-		  }
+		System.out.println(Arrays.toString(arrayDiff(new int[] { 1, 2 }, new int[] { 1 })));
+		System.out.println(Arrays.toString(arrayDiff(new int[] { 1, 2 }, new int[] { 1 })));
 
-	    public static void main(String[] args) {
-//	        int n[] = new int[7];
-
-	        // Assign values to individual elements
-//	        n[0] = 10;
-//	        n[1] = 20;
-//	        n[2] = 30;
-//	        n[3] = 40;
-//	        n[4] = 50;
-//	        n[5] = 60;
-//	        n[6] = 70;
-
-	        // Print the entire array using Arrays.toString()
-
-	        int[] throw1 = {5, 1, 3, 4, 1};
-	        int[] throw2 = {1, 1, 1, 3, 1};
-	        int[] throw3 = {2, 4, 4, 5, 4};
-
-	        System.out.println("Throw 1 Score: " + greedy(throw1)); // Should return 250
-	        System.out.println("Throw 2 Score: " + greedy(throw2)); // Should return 1100
-	        System.out.println("Throw 3 Score: " + greedy(throw3)); // Should return 450
-	    	
-	    }
+	}
 }
 
 //int number = 0;
 
-
- //i=0
+// i=0
 //-----------------
 //number=2
 
@@ -62,5 +42,3 @@ public class Main {
 
 //i=4
 //-----------------
-
-
